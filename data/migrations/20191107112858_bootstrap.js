067572748b52e558ec57492a4f6302d66738c0ef
Forrest-Darabian <mr.smiley72@yahoo.com>
2019-11-07 15:16:52 -0800

    migrations done

--- data/migrations/20191107112858_bootstrap.js
+++ data/migrations/20191107112858_bootstrap.js
@@ -12,11 +12,38 @@ exports.up = function(knex) {
         .unsigned()
         .references("recipe_id")
         .inTable("recipes_ingredients");
+    })
+    .createTable("recipes_ingredients", tbl => {
+      tbl.increments();
+      tbl
+        .integer("recipe_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("recipes");
+      tbl
+        .integer("ingredient_id")
+        .unsigned()
+        .references("id")
+        .inTable("ingredients");
+      tbl.integer("quantity").notNullable();
+    })
+    .createTable("instructions", tbl => {
+      tbl.increments();
+      tbl.integer("step_number").notNullable();
+      tbl.string("instructions").notNullable();
+      tbl
+        .integer("recipe_id")
+        .unsigned()
+        .references("id")
+        .inTable("recipes");
     });
 };
 
 exports.down = function(knex) {
   return knex.schema
     .dropTableIfExists("recipes_ingredients")
-    .dropTableIfExists("ingredients");
+    .dropTableIfExists("ingredients")
+    .dropTableIfExists("instructions")
+    .dropTableIfExists("recipes");
 };
