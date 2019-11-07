c4cdd7a1ba347006984c30e1077a76e0a3722dc9
Forrest-Darabian <mr.smiley72@yahoo.com>
2019-11-07 14:58:12 -0800

    working thru migrations

--- data/migrations/20191107112858_bootstrap.js
+++ data/migrations/20191107112858_bootstrap.js
@@ -1,8 +1,22 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema
+    .createTable("recipes", tbl => {
+      tbl.increments();
+      tbl.string("title").notNullable();
+    })
+    .createTable("ingredients", tbl => {
+      tbl.increments();
+      tbl.string("name").notNullable();
+      tbl
+        .integer("recipe_id")
+        .unsigned()
+        .references("recipe_id")
+        .inTable("recipes_ingredients");
+    });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema
+    .dropTableIfExists("recipes_ingredients")
+    .dropTableIfExists("ingredients");
 };
